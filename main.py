fb7754245e68eccee1bd2c21c83e88a77f6b0389
billy <zygboom@163.com>
2023-10-28 00:28:05 +0800

    update browser launch

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 from asyncio import get_event_loop
-
+import pyppeteer
 from wallhaven import Wallhaven
 
 
 async def main():
-    base_url = 'https://wallhaven.cc/'
+    base_url = 'https://wallhaven.cc/search?topRange=1y&order=desc&sorting='
+    browser = await pyppeteer.launch()
+    page = await browser.newPage()
+    await page.setViewport({'width': 1920, 'height': 1080})
     print('选择要下载的类别: ')
     print('1. TopList')
     print('2. Latest')
     print('3. Random')
     i = int(input('请输入: '))
-    url = None
     if i == 1:
-        url = base_url + 'toplist'
+        base_url = base_url + 'toplist'
+        await page.goto(base_url)
     elif i == 2:
-        url = base_url + 'latest'
+        base_url = base_url + 'latest'
+        await page.goto(base_url)
     elif i == 3:
-        url = base_url + 'random'
+        base_url = base_url + 'random'
+        await page.goto(base_url)
     else:
         print('无效的输入!【1，2，3】')
         exit(0)
+    wall = Wallhaven(page)
     page_start = int(input('从第几页开始下载: '))
     page_end = int(input('下载到第几页(最大只能下载10页): '))
+    wall_paper_path = '/Users/billy/Library/Mobile Documents/com~apple~CloudDocs/图片/壁纸'
     if page_end > 10 or page_end < page_start or page_start < 1:
         print('输入参数错误!')
         exit(0)
     for i in range(page_start, page_end + 1):
         print('正在下载第' + str(i) + '页')
-        final_url = url + '?page=' + str(i)
-        wall = Wallhaven(final_url)
-        await wall.get_images()
+        await wall.get_images(wall_paper_path)
         await wall.download_images()
+        await wall.next_page(base_url + '&page=' + str(i + 1))
+    await page.close()
+    await browser.close()
 
 
 if __name__ == '__main__':
--- wallhaven.py
+++ wallhaven.py
@@ -1,32 +1,23 @@
 import os
-
-import pyppeteer
 from bs4 import BeautifulSoup
 from requests import get
 
 
 class Wallhaven:
-    def __init__(self, url):
-        self.url = url
-        self.browser = None
-        self.page = None
+    def __init__(self, page):
+        self.page = page
         self.images = []
+        self.html = None
 
-    async def init(self):
-        self.browser = await pyppeteer.launch()
-        self.page = await self.browser.newPage()
-        await self.page.goto(self.url)
-
-    async def get_images(self):
-        await self.init()
+    async def get_images(self, exist_path):
         await self.page.waitForSelector('.thumb-listing-page ul li figure img')
-        for i in range(1, 10):
-            await self.page.evaluate(pageFunction='window.scrollTo(0, 1000)')
         html = await self.page.content()
+        self.html = html
         soup = BeautifulSoup(html, 'html.parser')
         images = soup.select('.thumb-listing-page ul li figure')
         thumb_info = soup.select('.thumb-listing-page ul li figure .thumb-info')
         final_images = []
+        wall_paper = os.listdir(exist_path)
         for image in images:
             is_png = image.select('.thumb-info .png')
             if is_png:
@@ -38,8 +29,8 @@ class Wallhaven:
             split_url = img_url.split('/')
             pre_split_url = img_url.split('/full/')
             image_name = '/' + split_url[-2] + '/wallhaven-' + split_url[-1]
-            final_images.append(pre_split_url[0] + '/full' + image_name)
-        await self.page.close()
+            if ('wallhaven-' + split_url[-1]) not in wall_paper:
+                final_images.append(pre_split_url[0] + '/full' + image_name)
         print('共有' + str(len(final_images)) + '张图片')
         self.images = final_images
 
@@ -47,11 +38,15 @@ class Wallhaven:
         # 检测 images 文件夹是否存在
         if not os.path.exists('./images'):
             os.mkdir('./images')
+        if len(self.images) == 0:
+            print('没有图片可以下载!')
+        else:
+            for image in self.images:
+                filename = image.split('/')[-1]
+                r = get(image).content
+                with open('./images/' + filename, 'wb') as f:
+                    f.write(r)
+                print('Downloaded: ' + filename)
 
-        for image in self.images:
-            filename = image.split('/')[-1]
-            r = get(image).content
-            with open('./images/' + filename, 'wb') as f:
-                f.write(r)
-            print('Downloaded: ' + filename)
-        await self.browser.close()
+    async def next_page(self, url):
+        await self.page.goto(url)
